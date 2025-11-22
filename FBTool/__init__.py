98bacd9a6881625b04608f2d5ed5e2d0d6ff2910
rafaykhan72242 <rafaykhan72242@gmail.com>
2025-11-22 12:28:20 +0500

    Update __init__.py

--- FBTool/__init__.py
+++ FBTool/__init__.py
@@ -1,7 +1,8 @@
 from .core import Start
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "Rafay Khan"
 __description__ = "Facebook Automation Tool for Specific Post Commenting"
+__license__ = "MIT"
 
 __all__ = ['Start']
